0e5886c29ed805bcd97048a697059e42f38f7e18
achyu <achyu@KAchyuth>
2025-07-27 15:40:24 +0530

    Initial Commit

---
+++ test/src/day1/demo3.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a=true;
+		boolean b=false;
+		
+		int x=7;
+		int y=9;
+		int z=x^y;
+		System.out.println("a&&b="+(a&&b));
+		System.out.println("a||b="+(a||b));
+		System.out.println("!(a&&b)="+!(a&&b));
+		System.out.println("z="+z);
+		
+		int i_val=10,j_val=2;
+		boolean chk;
+		chk=i_val<j_val;
+		System.out.println("chk value:""+chk);
+		
+	}
+
+}
